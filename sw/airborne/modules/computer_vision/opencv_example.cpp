9a20bef18dd90e77baf620d4d43387cc591196f2
smoggy-P <smoggy@sjtu.edu.cn>
2022-03-21 23:33:06 +0100

    camera parameter wrong

--- sw/airborne/modules/computer_vision/opencv_example.cpp
+++ sw/airborne/modules/computer_vision/opencv_example.cpp
@@ -49,69 +49,74 @@ int valid_labels[20] = {-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1
 Vec3b colors[20];
 int obs_num_detected = 0;
 
+Mat camera_matrix_ = (cv::Mat_<float>(3,3)<<290.0087, 0, 213.3118, 0, 290.6121, 272.5280, 0, 0, 1);
+Mat distor_coeffs = (cv::Mat_<float>(1,5) << -0.3098,  0.0933, 0, 0, -0.0127);
+
 
 int opencv_example(char *img, int width, int height){
   obs_num_detected = 0;
   Mat M(height, width, CV_8UC2, img);
+  Mat M1;
   
   cvtColor(M, image, COLOR_YUV2BGR_Y422);
 
-// TODO: undistort
+// TODO: undistor
+  undistort(image, M1, camera_matrix_, distor_coeffs);
 
+  // blur(image, image, Size(5, 5));
+  // // bilateralFilter(image, image_tmp, 15, 25, 25);
+  // cv::inRange(image, low, high, image1);
+  // cv::inRange(image, low2, high2, image2);
+  // cv::inRange(image, low3, high3, image3);
+  // image = 255 - (image2 | image1 | image3);
+  // for (int i=0;i<M.rows;i++)      
+  // {
+  //   for (int j=0;j<M.cols;j++)
+  //   {
+  //     if(j > M.cols/2){
+  //       image.at<uchar>(i,j)=0;
+  //     }
+  //   }
+  // }
+  // morphologyEx(image, src, MORPH_OPEN, kernel);
+  // labels     = Mat::zeros(src.size(), src.type());
+  // int num_labels = min(connectedComponentsWithStats(src, labels, stats, centroids, 4),20);
 
-  blur(image, image, Size(5, 5));
-  // bilateralFilter(image, image_tmp, 15, 25, 25);
-  cv::inRange(image, low, high, image1);
-  cv::inRange(image, low2, high2, image2);
-  cv::inRange(image, low3, high3, image3);
-  image = 255 - (image2 | image1 | image3);
-  for (int i=0;i<M.rows;i++)      
-  {
-    for (int j=0;j<M.cols;j++)
-    {
-      if(j > M.cols/2){
-        image.at<uchar>(i,j)=0;
-      }
-    }
-  }
-  morphologyEx(image, src, MORPH_OPEN, kernel);
-  labels     = Mat::zeros(src.size(), src.type());
-  int num_labels = min(connectedComponentsWithStats(src, labels, stats, centroids, 4),20);
+  // // Filtering
+  // for (int i = 0; i < num_labels; i++) {
+  //   if (!(stats.at<int>(i, CC_STAT_WIDTH) < 30 || stats.at<int>(i, CC_STAT_HEIGHT) < 30 || float(width / height) > 5 || stats.at<int>(i, CC_STAT_AREA) < 1000)) {
+  //     valid_labels[i] = i;
+  //     obs_num_detected++;
+  //   }
+  // }
+  // // generate background color => black
+  // colors[0] = Vec3b(0, 0, 0);
+  // // generate region color => random
+  // for (int i = 1; i < num_labels; i++) {
+  //   colors[i] = Vec3b(rng.uniform(0, 256), rng.uniform(0, 256), rng.uniform(0, 256));
+  // }
+  // // display components, drawing
+  // dst1 = Mat::zeros(src.size(), CV_8UC3);
+  // for (int row = 0; row < src.rows; row++) {
+  //   for (int col = 0; col < src.cols; col++) {
+  //     if (valid_labels[labels.at<int>(row, col)] == -1) {
+  //       continue;
+  //     }
+  //     dst1.at<Vec3b>(row, col) = colors[labels.at<int>(row, col)];
+  //   }
+  // }
 
-  // Filtering
-  for (int i = 0; i < num_labels; i++) {
-    if (!(stats.at<int>(i, CC_STAT_WIDTH) < 30 || stats.at<int>(i, CC_STAT_HEIGHT) < 30 || float(width / height) > 5 || stats.at<int>(i, CC_STAT_AREA) < 1000)) {
-      valid_labels[i] = i;
-      obs_num_detected++;
-    }
-  }
-  // generate background color => black
-  colors[0] = Vec3b(0, 0, 0);
-  // generate region color => random
-  for (int i = 1; i < num_labels; i++) {
-    colors[i] = Vec3b(rng.uniform(0, 256), rng.uniform(0, 256), rng.uniform(0, 256));
-  }
-  // display components, drawing
-  dst1 = Mat::zeros(src.size(), CV_8UC3);
-  for (int row = 0; row < src.rows; row++) {
-    for (int col = 0; col < src.cols; col++) {
-      if (valid_labels[labels.at<int>(row, col)] == -1) {
-        continue;
-      }
-      dst1.at<Vec3b>(row, col) = colors[labels.at<int>(row, col)];
-    }
-  }
-
-  // static and drawing
-  for (int i = 1; i < num_labels; i++) {
-    if(stats.at<int>(i, CC_STAT_AREA) > 1000){
-      circle(dst1, Point(centroids.at<Vec2d>(i, 0)[0], centroids.at<Vec2d>(i, 0)[1]), 2, Scalar(0, 0, 255), -1, 8, 0);         //中心点坐标
-      rectangle(dst1, Rect(stats.at<int>(i, CC_STAT_LEFT), stats.at<int>(i, CC_STAT_TOP), stats.at<int>(i, CC_STAT_WIDTH), stats.at<int>(i, CC_STAT_HEIGHT)), Scalar(255, 0, 255), 1, 8, 0);  //外接矩形
-    }
+  // // static and drawing
+  // for (int i = 1; i < num_labels; i++) {
+  //   if(stats.at<int>(i, CC_STAT_AREA) > 1000){
+  //     circle(dst1, Point(centroids.at<Vec2d>(i, 0)[0], centroids.at<Vec2d>(i, 0)[1]), 2, Scalar(0, 0, 255), -1, 8, 0);         //中心点坐标
+  //     rectangle(dst1, Rect(stats.at<int>(i, CC_STAT_LEFT), stats.at<int>(i, CC_STAT_TOP), stats.at<int>(i, CC_STAT_WIDTH), stats.at<int>(i, CC_STAT_HEIGHT)), Scalar(255, 0, 255), 1, 8, 0);  //外接矩形
+  //     printf("obstacle pos: %f   %f\n", centroids.at<Vec2d>(i, 0)[0], centroids.at<Vec2d>(i, 0)[1]);
+  //   }
     
-  }
+  // }
   
-  colorbgr_opencv_to_yuv422(dst1, img, width, height);
+  colorbgr_opencv_to_yuv422(M1, img, width, height);
 
   AbiSendMsgOBSTACLE_ESTIMATION(1,obs_num_detected,0,0,0,0,0,0,0,0,0,0);
   return 0;
